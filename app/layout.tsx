c54f6ae5798b15bce508b57fb77c69157382cb96
acrosson <crossonaj@gmail.com>
2025-04-16 14:55:16 -0400

    Added Reddit pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,6 +57,15 @@ export default function RootLayout({
           `,
         }}
       />
+      <Script
+        id="redditPixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_gue9rxvmwf9b');rdt('track', 'PageVisit');
+          `,
+        }}
+      />
     </html>
   );
 }
